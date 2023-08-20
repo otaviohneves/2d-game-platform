b85aeedb65e82d1460a65f1bc21685850b665da0
Igor Fernandes <igorfercontato@gmail.com>
2023-08-20 05:53:32 -0300

    Aula 63

--- scenes/UI/AnimatedButton.gd
+++ scenes/UI/AnimatedButton.gd
@@ -5,20 +5,25 @@ export(bool) var disableHoverAnim
 func _ready():
 	connect("mouse_entered", self, "on_mouse_entered")
 	connect("mouse_exited", self, "on_mouse_exited")
+	connect("focus_exited", self, "on_focus_exited")
 	connect("pressed", self, "on_pressed")
 
 func _process(delta):
 	rect_pivot_offset = rect_min_size / 2
 
+func reset_button_state():
+	if (!disableHoverAnim):
+		$HoverAnimationPlayer.play_backwards("hover")
+
 func on_mouse_entered():
 	if (!disableHoverAnim):
 		$HoverAnimationPlayer.play("hover")
 
-
 func on_mouse_exited():
-	if (!disableHoverAnim):
-		$HoverAnimationPlayer.play_backwards("hover")
+	reset_button_state()
 
+func on_focus_exited():
+	reset_button_state()
 
 func on_pressed():
 	$AudioStreamPlayer.play()
